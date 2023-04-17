4d9d2ee690d313d9ea8e42fe14d549f4687e0629
Tarek-DG <63122614+Tarek-DG@users.noreply.github.com>
2023-04-17 14:44:31 +0100

    up

--- lib/src/trader/screens/home/clients/clients_screen.dart
+++ lib/src/trader/screens/home/clients/clients_screen.dart
@@ -55,167 +55,172 @@ class _ClientItemState extends State<ClientItem> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: GestureDetector(
-        onTap: ()=>setState(() {
-          _open = !_open;
-        }),
-        child: Column(
-crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              width: width,
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.all(Radius.circular(24)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade200,
-                    spreadRadius: 5,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  SizedBox(width: 6,),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      padding: EdgeInsets.symmetric(vertical: 16 , horizontal: 18),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(24)),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Abdel Rahmen " , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 22 , fontWeight: FontWeight.w400),),
-                          const SizedBox(height: 12,),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children:  [
-                              const SizedBox(width: 3,),
-                              SizedBox(
-                                width: 16,
-                                height: 16,
-                                child: Image.asset("assets/icons/phone.png", color: Colors.deepPurple,),),
-
-                              const SizedBox(width: 17,),
-                              const Text("0778854321" ,
-                                style:  TextStyle(
-                                    fontSize: 15.5,
-                                    color: Colors.black54
-                                ),)
-                            ],
-                          ),
-                          SizedBox(height: 10,),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children:  [
-                              const SizedBox(width: 2,),
-                              SizedBox(
-                                width: 17.5,
-                                height: 17.5,
-                                child: Image.asset("assets/icons/location.png", color: Colors.deepPurple,),),
-                              const SizedBox(width: 16,),
-                              const Text("Blida, Blida" ,
-                                style:  TextStyle(
-                                    fontSize: 15.5,
-                                    color: Colors.black54
-                                ),),
-
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                ],
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            width: width,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade200,
+                  spreadRadius: 5,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
+                ),
+              ],
             ),
-            if(_open)...[
-              SizedBox(height: 15,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(width: 5,),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+            child: Row(
+              children: [
+                SizedBox(width: 6,),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(vertical: 16 , horizontal: 18),
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(.45),
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),
-                        ),
-                        BoxShadow(
-                          color: Colors.white,
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),),
-                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(24)),
                     ),
-                    child:  Icon(CupertinoIcons.phone_fill,color: Colors.deepPurple,size: 26,),
-                  ),
-                  SizedBox(width: 24,),
-                  Container(
-                    padding: EdgeInsets.symmetric( horizontal: 20, vertical: 8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(.425),
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("AbdelMoumen " , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 22 , fontWeight: FontWeight.w400),),
+                            GestureDetector(
+                                onTap: ()=>setState(() {
+                                  _open = !_open;
+                                }),
+                                child: Icon(Icons.more_horiz , color: Colors.deepPurple, size: 26,))
+                          ],
                         ),
-                        BoxShadow(
-                          color: Colors.white,
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),),
+                        const SizedBox(height: 16,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children:  [
+                            const SizedBox(width: 12,),
+                            SizedBox(
+                              width: 16,
+                              height: 16,
+                              child: Image.asset("assets/icons/phone.png", color: Colors.deepPurple,),),
 
-                      ],
-                    ),
-                    child:  Icon(Icons.comment,color: Colors.deepPurple,size: 26,),
-                  ),
-                  const SizedBox(width: 24,),
-                  Container(
-                    padding: EdgeInsets.symmetric( horizontal: 20, vertical: 8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(.425),
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),
+                            const SizedBox(width: 16,),
+                            const Text("0778854321" ,
+                              style:  TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.black54
+                              ),)
+                          ],
                         ),
-                        BoxShadow(
-                          color: Colors.white,
-                          spreadRadius: 1,
-                          blurRadius: 8,
-                          offset: Offset(-2, 2),),
+                        SizedBox(height: 12,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children:  [
+                            const SizedBox(width: 12,),
+                            SizedBox(
+                              width: 17.5,
+                              height: 17.5,
+                              child: Image.asset("assets/icons/location.png", color: Colors.deepPurple,),),
+                            const SizedBox(width: 16,),
+                            const Text("Blida, Blida" ,
+                              style:  TextStyle(
+                                  fontSize: 15.5,
+                                  color: Colors.black54
+                              ),),
 
+                          ],
+                        ),
                       ],
                     ),
-                    child:  Icon(CupertinoIcons.location_solid,color: Colors.deepPurple,size: 26,),
                   ),
-                  SizedBox(width: 8,),
-                ],
-              ),
-              SizedBox(height: 10,),
-            ]
-          ],
-        ),
+                ),
+
+              ],
+            ),
+          ),
+          if(_open)...[
+            SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(width: 5,),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.45),
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),
+                      ),
+                      BoxShadow(
+                        color: Colors.white,
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),),
+                    ],
+                  ),
+                  child:  Icon(CupertinoIcons.phone_fill,color: Colors.deepPurple,size: 26,),
+                ),
+                SizedBox(width: 24,),
+                Container(
+                  padding: EdgeInsets.symmetric( horizontal: 20, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.425),
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),
+                      ),
+                      BoxShadow(
+                        color: Colors.white,
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),),
+
+                    ],
+                  ),
+                  child:  Icon(Icons.comment,color: Colors.deepPurple,size: 26,),
+                ),
+                const SizedBox(width: 24,),
+                Container(
+                  padding: EdgeInsets.symmetric( horizontal: 20, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.425),
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),
+                      ),
+                      BoxShadow(
+                        color: Colors.white,
+                        spreadRadius: 1,
+                        blurRadius: 8,
+                        offset: Offset(-2, 2),),
+
+                    ],
+                  ),
+                  child:  Icon(CupertinoIcons.location_solid,color: Colors.deepPurple,size: 26,),
+                ),
+                SizedBox(width: 8,),
+              ],
+            ),
+            SizedBox(height: 10,),
+          ]
+        ],
       ),
     );
   }
