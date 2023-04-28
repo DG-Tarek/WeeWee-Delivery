3dfa5404c003ca5c58e2b43d102ef5fc2ef39535
Tarek-DG <63122614+Tarek-DG@users.noreply.github.com>
2023-04-28 14:52:25 +0100

    up

---
+++ lib/src/trader/screens/home/stock/product_history_screen.dart
@@ -0,0 +1,155 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weewee_delivery/src/trader/screens/home/stock/stock_screen.dart';
+
+import '../../../../moduls/shared/package_model.dart';
+import '../../../../moduls/trader/product_model.dart';
+
+class ProductHistoryScreen extends StatelessWidget {
+  const ProductHistoryScreen({Key? key, required this.product}) : super(key: key);
+  final Product product;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 8.h,),
+          StockItem(product: product),
+          SizedBox(height: 10.h,),
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 24.0),
+             child: Text("Product Sales History" , style: Theme.of(context).textTheme.titleLarge,),
+           ),
+          const SizedBox(height: 5),
+          ProductHistoryItem(),
+          ProductHistoryItem(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ProductHistoryItem extends StatelessWidget {
+  const ProductHistoryItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10, left: 16),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade200,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Kaddour" ,style: TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.w500),  ),
+                          SizedBox(height: 5,),
+                          Text("05565656" ,style: TextStyle(fontSize: 14 , fontWeight: FontWeight.w300 , color: Colors.black54), ),
+                          Text("05565656" ,style: TextStyle(fontSize: 14 , fontWeight: FontWeight.w300, color: Colors.black45), ),
+
+                        ],)),
+                  Expanded(
+                      child: Container(
+                        alignment: FractionalOffset.centerLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("23" ,style: TextStyle(fontSize: 22 , fontWeight: FontWeight.w400, color: Colors.black54),  ),
+                            Text("Avril" ,style: TextStyle(fontSize: 14 , fontWeight: FontWeight.w400, color: Colors.black54),  ),
+                            Text("2023" ,style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black54),  ),
+
+                          ],),
+                      )),
+                  Expanded(
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 15,),
+                        Row(
+                          children: const [
+                             Text("Quantity" ,style: TextStyle(fontSize: 12 , fontWeight: FontWeight.w400, color: Colors.black54),  ),
+                            SizedBox(width: 8,),
+                             Text("1" ,style: TextStyle(fontSize: 18 , fontWeight: FontWeight.w600, color: Colors.green),  ),
+                          ],
+                        ),
+                        const SizedBox(height: 2,),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text("2300" ,style: TextStyle(fontSize: 20 , fontWeight: FontWeight.w400 , color: Colors.red),  ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 6.0),
+                              child: Text(" DZ" ,style: TextStyle(fontSize: 12 , color: Colors.red),  ),
+                            ),
+                          ],
+                        ),
+
+                      ],),
+                  )),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              top: 0,
+              right: 8,
+              child: Container(
+
+                padding: const EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                  color:  Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, -2),
+                    ),
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(2, 0),
+                    ),
+
+                    const BoxShadow(
+                      color: Colors.white,
+                      spreadRadius: 3,
+                      blurRadius: 0,
+                      offset: Offset(-2, 2),)
+                  ],
+                ),
+                child:  Text("23" ,style: TextStyle(fontSize: 14 , fontWeight: FontWeight.w600, color: Colors.deepPurple),  ),
+
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/src/trader/screens/home/stock/stock_screen.dart
+++ lib/src/trader/screens/home/stock/stock_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:weewee_delivery/src/moduls/trader/product_model.dart';
 import 'package:weewee_delivery/src/trader/provider/trader_firebase_cubit_states.dart';
+import 'package:weewee_delivery/src/trader/screens/home/stock/product_history_screen.dart';
 
 import '../../../../constant/constant.dart';
 import '../../../provider/trader_firebase_cubit.dart';
@@ -51,6 +52,7 @@ class StockScreen extends StatelessWidget {
                   child: StockItem(
                 product: TraderFirebaseCubit().productsList[index],
                   selectIsAvailable: selectIsAvailable,
+                    showHistory: true,
                   ));
 
               }),
@@ -59,103 +61,153 @@ class StockScreen extends StatelessWidget {
     );
   }
 }
+
+
+
 class StockItem extends StatelessWidget {
-  const StockItem({Key? key, required this.product, this.selectIsAvailable = false}) : super(key: key);
-  final Product product ;
+  const StockItem(
+      {Key? key, required this.product, this.selectIsAvailable = false, this.showHistory = false})
+      : super(key: key);
+
+  final Product product;
   final bool selectIsAvailable;
+  final bool showHistory;
+
   @override
   Widget build(BuildContext context) {
-    final color =  COLORS[Random().nextInt(COLORS.length)];
-    return  Padding(
+    final color = COLORS[Random().nextInt(COLORS.length)];
+    return Padding(
       padding: const EdgeInsets.only(left: 16.0, bottom: 12),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
+      child: GestureDetector(
+          onTap: (){
+            if(showHistory){
+              Navigator.of(context).push(_createRoute());
+            }
+          },
+          child: Stack(
+          alignment: Alignment.center,
+          children: [
           Padding(
-            padding: const EdgeInsets.only(top: 16.0, right: 16),
-            child: GestureDetector(
-              onLongPress: (){
-                if(selectIsAvailable){
-                  if(product.stock>0){
-                    TraderFirebaseCubit().setFirstProductChoice(product);
-                    Navigator.of(context).pop();
-                  }else{
-                    debugPrint("This Product is out of your Stock");
-                  }
-                }
-              },
-              child: Container(
-                padding: EdgeInsets.only(left: 3),
-                decoration: BoxDecoration(
-                  color: color,
-                  borderRadius: BorderRadius.all(Radius.circular(24)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      spreadRadius: 3,
-                      blurRadius: 7,
-                      offset: Offset(3, 3),
-                    ),
+          padding: const EdgeInsets.only(top: 16.0, right: 16),
+      child: GestureDetector(
+        onLongPress: () {
+          if (selectIsAvailable) {
+            if (product.stock > 0) {
+              TraderFirebaseCubit().setFirstProductChoice(product);
+              Navigator.of(context).pop();
+            } else {
+              debugPrint("This Product is out of your Stock");
+            }
+          }
+        },
+        child: Container(
+          padding: EdgeInsets.only(left: 3),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade200,
+                spreadRadius: 3,
+                blurRadius: 7,
+                offset: Offset(3, 3),
+              ),
+            ],
+          ),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                    width: width - 40,
+                    child: Text(product.name, style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        height: 1.35),)),
+
+                const SizedBox(height: 16,),
+                Text(product.description,
+                    style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey, fontWeight: FontWeight.w300
+                    )),
+                const SizedBox(height: 14,),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("Price", style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.red),),
+                    const SizedBox(width: 12,),
+                    Text(product.price.toString(), style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black87),),
+                    const SizedBox(width: 4,),
+                    Text("DZ", style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black87),),
+                    Spacer(),
+                    Text("Stock", style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.red),),
+                    const SizedBox(width: 8,),
+                    Text("${product.stock} / ${product.minStock}", style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.brown),),
+
                   ],
                 ),
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.symmetric(vertical: 16 , horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(24)),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                          width: width - 40,
-                          child: Text(product.name , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 20, fontWeight: FontWeight.w400 , height: 1.35),)),
-
-                      const SizedBox(height: 16,),
-                      Text(product.description ,
-                        style:  const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey , fontWeight: FontWeight.w300
-                        )),
-                      const SizedBox(height: 14,),
-
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text("Price" , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 16, fontWeight: FontWeight.w300, color: Colors.red),),
-                          const SizedBox(width: 12,),
-                          Text(product.price.toString() , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black87),),
-                          const SizedBox(width: 4,),
-                          Text("DZ" , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black87),),
-                         Spacer(),
-                          Text("Stock" , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 12, fontWeight: FontWeight.w300, color: Colors.red),),
-                          const SizedBox(width: 8,),
-                          Text("${product.stock} / ${product.minStock}" , style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.brown),),
-
-                        ],
-                      ),
 
 
-                    ],
-                  ),
-
-                ),
-              ),
+              ],
             ),
+
           ),
+        ),
+      ),
+    ),
     Positioned(
     top: 0,
     right: 8,
     height: 60,
     width: 60,
-    child:  Container(
+    child: Container(
 
     padding: const EdgeInsets.only(left: 5,right: 7,top: 8,bottom: 4),
     decoration: BoxDecoration(
     color: product.stock< product.minStock ? Colors.red : Colors.white,
     shape: BoxShape.circle,
-    boxShadow:  <BoxShadow>[
+    boxShadow: <BoxShadow>[
     BoxShadow(
     color: Colors.grey.shade300,
     spreadRadius: 1,
@@ -168,22 +220,43 @@ class StockItem extends StatelessWidget {
     blurRadius: 5,
     offset: Offset(2, 0),
     ),
- product.stock >= product.minStock ?
+    product.stock >= product.minStock ?
     const BoxShadow(
     color: Colors.white,
     spreadRadius: 3,
     blurRadius: 0,
     offset: Offset(-2, 2),):
- const BoxShadow(
-   color: Colors.red,
-   spreadRadius: 1,
-   blurRadius: 0,
-   offset: Offset(-1, 1),),
+    const BoxShadow(
+    color: Colors.red,
+    spreadRadius: 1,
+    blurRadius: 0,
+    offset: Offset(-1, 1),),
     ],
     ),
     child: Image.asset("assets/icons/packagelogo.png"),
     ))],
-      ),
+    ),
+    ),
+    );
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation,
+          secondaryAnimation) => ProductHistoryScreen(product: product),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween = Tween(begin: begin, end: end).chain(
+            CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
     );
   }
 }
